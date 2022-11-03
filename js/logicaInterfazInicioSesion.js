a4dc5f44a27e502fa82db6c56bfa391d54123639
JuanCPoos <juan.cpoos@gmail.com>
2022-11-02 23:27:48 -0300

    Se incorpora admin y cliente html + func iniciar

--- js/logicaInterfazInicioSesion.js
+++ js/logicaInterfazInicioSesion.js
@@ -6,9 +6,31 @@ function iniciarSesion() {
     let bAcceso = false;
 
     sCorreo = document.querySelector('#txtCorreo').value;
-    sContraseña = document.querySelector('#txtContrasena').value;
+    sContrasena = document.querySelector('#txtContrasena').value;
 
     bAcceso = validarCredenciales(sCorreo, sContrasena);
-
     
-}
+    if (bAcceso == true) {
+        ingresar();
+    }
+
+   }
+    /* esta función se ejecuta cuando el acceso es correcto - 
+        switch -> si el rol es case 1, nos redirige a admin.html
+                -> si el rol es case 2 nos redirige a cliente.html */
+    function ingresar() { 
+        let rol = sessionStorage.getItem('rolUsuarioActivo');
+        switch (rol) {
+            case '1':
+                window.location.href = "admin.html";
+                break;
+            case '2':
+                window.location.href = "cliente.html";
+                break;
+            default:
+                window.location.href="index.html";
+                break;
+        }
+   }
+    
+
